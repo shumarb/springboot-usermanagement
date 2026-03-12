4876cfcd6655c791a6f6a7677a590c11db670f3b
Umar <shumarb@outlook.com>
2026-03-12 18:32:49 +0800

    Initial commit

---
+++ src/main/java/com/springboot/usermanagement/SpringbootUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUsermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUsermanagementApplication.class, args);
+	}
+
+}
